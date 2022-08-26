0312c9c141b5f96f2604664ecc686db940988362
Ermias <ermias.gkassa@gmail.com>
2022-08-26 13:04:08 -0700

    Push all filter and delete image codes

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,20 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
+import { emitWarning } from 'process';
+import fs from "fs";
 
 (async () => {
-
   // Init the Express application
   const app = express();
-
   // Set the network port
-  const port = process.env.PORT || 8082;
-  
+  const port = process.env.PORT || 3001;
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-
   // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
   // endpoint to filter an image from a public url.
   // IT SHOULD
-  //    1
   //    1. validate the image_url query
   //    2. call filterImageFromURL(image_url) to filter the image
   //    3. send the resulting file in the response
@@ -32,20 +29,26 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   //! END @TODO1
 
   // app.get("/", async (req, res) => {
-
-  //   let {filteredimage} = req.query;
-
-  //       // check to make sure the id is set
-  //   if (!filteredimage) { 
-  //         // respond with an error if not
-  //         return res.status(400).send(`Image is required`);
-  //   }
   //   res.send("Show the variable query "+filteredimage)
   // } ); 
   // Root Endpoint
   // Displays a simple message to the user
-  app.get( "/", async ( req, res ) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+
+  app.get( "/filteredimage", async ( req, res ) => {
+
+    let {image_url} = req.query;
+    // check to make sure the image url is set
+    if (!image_url) { 
+      // respond with an error if not
+      return res.status(400).send(`Image URL is required`);
+    }
+    let filteredimage =await filterImageFromURL(image_url);
+
+    res.status(200).sendFile(filteredimage);
+      fs.readdir(__dirname+'/util/tmp', (err,files)=>{ deleteLocalFiles(files)  }
+      )
+
+
   } );
   
 
--- src/util/util.ts
+++ src/util/util.ts
@@ -1,29 +1,31 @@
+import { response } from "express";
 import fs from "fs";
 import Jimp = require("jimp");
-
-// filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
-// INPUTS
-//    inputURL: string - a publicly accessible url to an image file
+// INPUTS ==>   inputURL: string - a publicly accessible url to an image file
 // RETURNS
 //    an absolute path to a filtered image locally saved file
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
+   console.log("This filterImageFromURL is inputURL==>"+inputURL);
     try {
       const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
-      await photo
+      const outpath = "/tmp/filteredLions." + Math.floor(Math.random() * 2000) + ".jpg";
+      await photo 
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
         .write(__dirname + outpath, (img) => {
           resolve(__dirname + outpath);
-        });
+        }); 
     } catch (error) {
-      reject(error);
+     // reject(error);
+     console.log("Somthing wrong with the Image URL "+error);
+     //response.status(404).send("Somthing wrong with the Image URL "+error)
+     
     }
+    //console.log("This is outpath in filterImageFromURL function==>"+outpath);
   });
 }
 
@@ -34,6 +36,6 @@ export async function filterImageFromURL(inputURL: string): Promise<string> {
 //    files: Array<string> an array of absolute paths to files
 export async function deleteLocalFiles(files: Array<string>) {
   for (let file of files) {
-    fs.unlinkSync(file);
+    fs.unlinkSync(__dirname+'/tmp/'+file);
   }
 }
