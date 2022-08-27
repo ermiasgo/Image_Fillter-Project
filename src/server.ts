b7db036cac4e029c5e3a5f8b199db19aa3f945af
Ermias <ermias.gkassa@gmail.com>
2022-08-27 14:09:32 -0700

    Add deployment image

--- src/server.ts
+++ src/server.ts
@@ -11,28 +11,6 @@ import fs from "fs";
   const port = process.env.PORT || 3001;
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  // app.get("/", async (req, res) => {
-  //   res.send("Show the variable query "+filteredimage)
-  // } ); 
-  // Root Endpoint
-  // Displays a simple message to the user
 
   app.get( "/filteredimage", async ( req, res ) => {
 
@@ -47,10 +25,11 @@ import fs from "fs";
     res.status(200).sendFile(filteredimage);
       fs.readdir(__dirname+'/util/tmp', (err,files)=>{ deleteLocalFiles(files)  }
       )
-
-
   } );
   
+  app.get( "/", async ( req, res ) => {
+    res.status(200).send("Please try GET /filteredimage?image_url={{}}")
+  } );
 
   // Start the Server
   app.listen( port, () => {
